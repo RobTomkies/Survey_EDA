c0db36aa18de48e75cf7f71813d40d5c1fe4094a
RobTomkies <63220353+RobTomkies@users.noreply.github.com>
2022-10-03 17:29:43 +1300

    s3 print function, dplyr for repeat rows

--- SurveyEdaPackage/R/Data_Describe.R
+++ SurveyEdaPackage/R/Data_Describe.R
@@ -1,21 +1,23 @@
+
+#' @export
 Data_Describe <- function(dataset,
-                          NLP_force = c(),
-                          ordinal_force = list(), #list(c(‘colname’, ‘level1’, ‘level2’))
-                          nominal_force = c(),
-                          numeric_force = c(),
-                          alternate_nas = list(), #list(c(“colname1”, 0, 99),c(“colname2”, “hold”))
-                          preserve_nonconform = T,
-                          remove_repeat = T){
+                          NLP.force = c(),
+                          ordinal.force = list(), #list(c(‘colname’, ‘level1’, ‘level2’))
+                          nominal.force = c(),
+                          numeric.force = c(),
+                          alternate.nas = list(), #list(c(“colname1”, 0, 99),c(“colname2”, “hold”))
+                          preserve.nonconform = T,
+                          remove.repeat = T){
   #dataset dimensions
   original_nrow <- nrow(dataset)
   original_ncol <- ncol(dataset)
 
 
   #detect and correct for data types, includes many error catching steps.
-  updated_data <- data_type_detect(dataset, NLP_force = NLP_force, ordinal_force = ordinal_force,
-                   nominal_force = nominal_force, numeric_force = numeric_force,
-                   numeric_force = numeric_force, alternate_nas = alternate_nas,
-                   preserve_nonconform = preserve_nonconform)
+  updated_data <- data_type_detect(dataset, NLP_force = NLP.force, ordinal_force = ordinal.force,
+                   nominal_force = nominal.force, numeric_force = numeric.force,
+                   alternate_nas = alternate.nas,
+                   preserve_nonconform = preserve.nonconform)
   #clean up original input data
   rm(dataset)
 
@@ -25,6 +27,12 @@ Data_Describe <- function(dataset,
 
   #memory
   memory_size <- object.size(updated_data$data)
+
+  #number of identicle rows
+  if(remove.repeat == T){
+    updated_data <- dplyr::distinct(updated_data)
+  }
+
 
   #datatypes
   data_types <- cbind( updated_data$original_type['data_type'], updated_data$converted_type['data_type'])
@@ -37,16 +45,13 @@ Data_Describe <- function(dataset,
   Float_type <- updated_data$converted_type$data_field[updated_data$converted_type$data_type == 'Float']
   Nominal_type <- updated_data$converted_type$data_field[updated_data$converted_type$data_type == 'Nominal']
 
-  ML <- max(sapply(list(Ordinal_type, NLP_type, Integer_type, Float_type, Nominal_type)), length)
+  ML <- max(sapply(list(Ordinal_type, NLP_type, Integer_type, Float_type, Nominal_type), length))
   #pad vectors
   length(Ordinal_type) <- length(NLP_type)<- length(Integer_type)<- length(Float_type)<- length(Nominal_type) <- ML
   #bind
   dt_type_frame <- cbind(Ordinal_type, NLP_type, Integer_type, Float_type, Nominal_type)
 
-  #set NA values to invisible for print
-#!TODO - maybe shift to a s3 function
-  output_dt_type <- dt_type_frame
-  output_dt_type[is.na(output_dt_type)] <- ""
+
 
   #missingness
   missingness_vector <- apply(updated_data$data, 2,  function(x){return(100*(sum(is.na(x)))/length(x))})
@@ -58,7 +63,7 @@ Data_Describe <- function(dataset,
   mt50_u75 <- names(missingness_vector)[missingness_vector <= 75 & missingness_vector > 50]
   mt75 <- names(missingness_vector)[missingness_vector > 75]
 
-  ML <- max(sapply(list(complete, u10,mt10_25, mt25_u50, mt50_u75, mt75)), length)
+  ML <- max(sapply(list(complete, u10,mt10_25, mt25_u50, mt50_u75, mt75), length))
   #pad vectors
   length(complete) <- length(u10)<- length(mt10_25)<- length(mt25_u50)<- length(mt50_u75)<- length(mt75) <- ML
   #bind
@@ -77,4 +82,16 @@ Data_Describe <- function(dataset,
   return(output)
 }
 
+#' @export
+print.data_describe <- function(x){
+  x <- unclass(x)
+  pander(x$dimensions)
 
+  output_dt_type <- x$data_type_names
+  output_dt_type[is.na(output_dt_type)] <- ""
+  pander(output_dt_type)
+
+  output_dt_mis <- x$grouped_missingness
+  output_dt_mis[is.na(output_dt_mis)] <- ""
+  pander(output_dt_mis)
+}
